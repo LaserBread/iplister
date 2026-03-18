2de885d8edb75d4acb6c8d2336379b76cfa54471
Ethan <ej.laserbread@proton.me>
2026-03-17 18:34:02 -0700

    Made Host Class

--- app/src/main/java/edu/oregonstate/joneset3/iplist/data/Host.kt
+++ app/src/main/java/edu/oregonstate/joneset3/iplist/data/Host.kt
@@ -4,7 +4,10 @@ import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 import inet.ipaddr.IPAddressString
 import inet.ipaddr.ipv4.IPv4Address
+import okhttp3.internal.toImmutableList
 
+val validateHostname = Regex("^(?![0-9]+$)(?!.*-$)(?!-)[a-zA-Z0-9-]{1,63}$")
+val validateMAC =      Regex("^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$")
 
 @JsonClass(generateAdapter = true)
 /* A host on a network with its IP addresses and all. Holds a list of attributes that correlates to
@@ -14,13 +17,46 @@ import inet.ipaddr.ipv4.IPv4Address
  *
  * @param name The name the app uses to refer to this host. It's not the same as the hostname.
  *
- * @param ipv4 The host's IPv4 address. Credit to Sean Foley's IPAddress library
+ * @param ipv4 The host's IPv4 address.
+ *
+ * @param cidr The IPv4 CIDR mask.
+ *
  */
 data class Host(
     val name: String,
     val hostname: String?,
     val ipv4: IPv4Address?,
-    @Json(name = "cidrmask") val cidr: Int,
+    @Json(name = "cidrmask") val cidr: Int?,
     val mac: String?,
     val id: Int? = null
-)
+){
+    fun validate(): List<HostErrors>{
+        val errors: MutableList<HostErrors> = mutableListOf()
+
+        // Check the name is not empty
+        if (name == null || name == ""){
+            errors.add(HostErrors.NO_NAME)
+        }
+
+        if (name.length > 100){
+            errors.add(HostErrors.NAME_LONG)
+        }
+
+        // Check the hostname
+        if (hostname != null && !validateHostname.matches(hostname)){
+             errors.add(HostErrors.HOSTNAME_INVALID)
+        }
+
+        if (cidr != null && cidr !in 0..32){
+            errors.add(HostErrors.CIDR_INVALID)
+        }
+
+        if (mac != null && !validateMAC.matches(mac)){
+            errors.add(HostErrors.INVALID_MAC)
+        }
+
+
+        return errors.toImmutableList()
+    }
+}
+
---
+++ app/src/main/java/edu/oregonstate/joneset3/iplist/data/HostErrors.kt
@@ -0,0 +1,16 @@
+package edu.oregonstate.joneset3.iplist.data
+
+enum class HostErrors {
+    NO_NAME, // Name is missing
+    NAME_LONG, // Name is too long
+
+    NO_IDENTIFIER, // No identifier
+
+    HOSTNAME_INVALID, // Invalid Hostname
+
+    IPv4_INVALID, // Invalid IPv4 address
+
+    CIDR_INVALID, // Invalid CIDR
+
+    INVALID_MAC, // Invalid MAC Address
+}
